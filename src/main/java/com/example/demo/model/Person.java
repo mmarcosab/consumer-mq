3a339bcf5008710a5e175e779e0d68ae73f6e185
mmarcosa <marcos.barbosa8@fatec.sp.gov.br>
2020-05-15 03:24:47 -0300

    second commit

--- src/main/java/com/example/demo/model/Person.java
+++ src/main/java/com/example/demo/model/Person.java
@@ -1,10 +1,17 @@
 package com.example.demo.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 public class Person {
 
@@ -17,42 +24,6 @@ public class Person {
 	
 	@Column(name = "age")
 	private int age;
-
-	public Person() {
-
-	}
-	
-	public Person(String name, int age) {
-		super();
-		this.name = name;
-		this.age = age;
-	}
-
-	public Person(int id, String name, int age) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.age = age;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
 
 	@Override
 	public String toString() {
